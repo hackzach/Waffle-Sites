323c8c0e836999fb74c9690ad784213debe7439d
hackzach <zacharyp.com@gmail.com>
2012-01-30 10:24:47 -0600

    Update apps/authorize.php

--- apps/authorize.php
+++ apps/authorize.php
@@ -8,7 +8,7 @@ if(!$auth->isUser()) {
 else {
 	$scope = "read_stream,publish_stream";
 	ob_flush();
-	header("Location: https://www.facebook.com/dialog/oauth?client_id=177894922232847&client_secret=6e987ae3f1172c066b7f67f631c5e01c&redirect_uri=".$domain."/".addslashes(strip_tags($_REQUEST['ref']))."&scope=$scope");
+	header("Location: https://www.facebook.com/dialog/oauth?client_id=&client_secret=&redirect_uri=".$domain."/".addslashes(strip_tags($_REQUEST['ref']))."&scope=$scope");
 	ob_end_flush();
 }
 
